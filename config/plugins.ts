46ecc32136671c7f2e07c96cfd12481cae376557
Kacper Baranowski <dev@kbcode.pl>
2024-11-07 00:41:21 +0100

    fix for adding and configuring nodemailer

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default () => ({
+	email: {
+		config: {
+			provider: 'nodemailer',
+			providerOptions: {
+				host: process.env.SMTP_HOST,
+				port: process.env.SMTP_PORT,
+				auth: {
+					user: process.env.SMTP_USERNAME,
+					pass: process.env.SMTP_PASSWORD,
+				},
+				secure: true,
+			},
+			settings: {
+				defaultFrom: 'admin@galeriastarowicz.pl',
+				defaultReplyTo: 'admin@galeriastarowicz.pl',
+			},
+		},
+	},
+})
