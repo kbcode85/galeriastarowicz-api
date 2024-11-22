e9374aabdb3a8a39db8dba2f4dba6c906ec25b24
Kacper Baranowski <dev@kbdev.pl>
2024-11-22 23:10:14 +0100

    edit ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,19 +4,11 @@ module.exports = {
 			name: 'strapi-prod',
 			script: 'npm',
 			args: 'start',
-			env: {
-				NODE_ENV: 'production',
-				ENV_PATH: '.env.production',
-			},
 		},
 		{
 			name: 'strapi-dev',
 			script: 'npm',
 			args: 'develop',
-			env: {
-				NODE_ENV: 'development',
-				ENV_PATH: '.env.development',
-			},
 		},
 	],
 }
